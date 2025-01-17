a1f8986412f3e498963fb2620b5757ba71541da7
Vincenzo Palazzo <vincenzopalazzodev@gmail.com>
2025-01-17 18:29:51 +0100

    docs: remove bun
    Signed-off-by: Vincenzo Palazzo <vincenzopalazzodev@gmail.com>

--- .docusaurus/routes.js
+++ .docusaurus/routes.js
@@ -3,48 +3,83 @@ import ComponentCreator from '@docusaurus/ComponentCreator';
 
 export default [
   {
+    path: '/__docusaurus/debug',
+    component: ComponentCreator('/__docusaurus/debug', '936'),
+    exact: true
+  },
+  {
+    path: '/__docusaurus/debug/config',
+    component: ComponentCreator('/__docusaurus/debug/config', 'd63'),
+    exact: true
+  },
+  {
+    path: '/__docusaurus/debug/content',
+    component: ComponentCreator('/__docusaurus/debug/content', '3eb'),
+    exact: true
+  },
+  {
+    path: '/__docusaurus/debug/globalData',
+    component: ComponentCreator('/__docusaurus/debug/globalData', '0b2'),
+    exact: true
+  },
+  {
+    path: '/__docusaurus/debug/metadata',
+    component: ComponentCreator('/__docusaurus/debug/metadata', '39b'),
+    exact: true
+  },
+  {
+    path: '/__docusaurus/debug/registry',
+    component: ComponentCreator('/__docusaurus/debug/registry', '5b9'),
+    exact: true
+  },
+  {
+    path: '/__docusaurus/debug/routes',
+    component: ComponentCreator('/__docusaurus/debug/routes', '01a'),
+    exact: true
+  },
+  {
     path: '/blog',
-    component: ComponentCreator('/blog', 'c20'),
+    component: ComponentCreator('/blog', 'c97'),
     exact: true
   },
   {
     path: '/blog/archive',
-    component: ComponentCreator('/blog/archive', '1fd'),
+    component: ComponentCreator('/blog/archive', '803'),
     exact: true
   },
   {
     path: '/blog/tags',
-    component: ComponentCreator('/blog/tags', '454'),
+    component: ComponentCreator('/blog/tags', 'c63'),
     exact: true
   },
   {
     path: '/blog/tags/lampo',
-    component: ComponentCreator('/blog/tags/lampo', '0c6'),
+    component: ComponentCreator('/blog/tags/lampo', 'f68'),
     exact: true
   },
   {
     path: '/blog/tags/lightning-network',
-    component: ComponentCreator('/blog/tags/lightning-network', 'fc3'),
+    component: ComponentCreator('/blog/tags/lightning-network', 'c43'),
     exact: true
   },
   {
     path: '/blog/testing',
-    component: ComponentCreator('/blog/testing', '332'),
+    component: ComponentCreator('/blog/testing', '48d'),
     exact: true
   },
   {
     path: '/blog/v23.12',
-    component: ComponentCreator('/blog/v23.12', '4a0'),
+    component: ComponentCreator('/blog/v23.12', '20b'),
     exact: true
   },
   {
     path: '/blog/welcome',
-    component: ComponentCreator('/blog/welcome', 'e88'),
+    component: ComponentCreator('/blog/welcome', '46e'),
     exact: true
   },
   {
     path: '/docs',
-    component: ComponentCreator('/docs', '990'),
+    component: ComponentCreator('/docs', '224'),
     routes: [
       {
         path: '/docs/category/lampo-lightning-node---basics',
@@ -92,7 +127,7 @@ export default [
   },
   {
     path: '/',
-    component: ComponentCreator('/', '56a'),
+    component: ComponentCreator('/', 'e4e'),
     exact: true
   },
   {
